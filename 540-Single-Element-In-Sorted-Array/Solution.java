55b20b3fb0e6525fe3f170bc11c50094b1862298
YashBhut1524 <yashxerox15@gmail.com>
2025-06-15 21:57:34 +0530

    update Single Element in Sorted Array

--- 540-Single-Element-In-Sorted-Array/Solution.java
+++ 540-Single-Element-In-Sorted-Array/Solution.java
@@ -1,16 +1,28 @@
 public class Solution {
+    // public int singleNonDuplicate(int[] nums) {
+    // int left = 0;
+    // int right = nums.length - 1;
+    // while (left < right) {
+    // int mid = (left + right) / 2;
+
+    // if (mid % 2 == 1)
+    // mid--;
+    // if (nums[mid] == nums[mid + 1]) {
+    // left = mid + 2;
+    // } else {
+    // right = mid;
+    // }
+    // }
+    // return nums[left];
+    // }
     public int singleNonDuplicate(int[] nums) {
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
-
-            if (mid % 2 == 1)
-                mid--;
-            if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
-            } else {
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                left = mid + 1;
+            else
                 right = mid;
-            }
         }
         return nums[left];
     }
