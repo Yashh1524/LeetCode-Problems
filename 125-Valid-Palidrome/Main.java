ee54e7742277cd1c7f6139a642976dcf178f1762
YashBhut1524 <yashxerox15@gmail.com>
2025-06-19 21:35:39 +0530

    update 125

--- 125-Valid-Palidrome/Main.java
+++ 125-Valid-Palidrome/Main.java
@@ -1,19 +1,54 @@
 public class Main {
 
-    // Solution class with the isPalindrome method
+    // static class Solution {
+    // public boolean isPalindrome(String s) {
+    // s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+    // int left = 0;
+    // int right = s.length() - 1;
+
+    // while (left < right) {
+    // if (s.charAt(left) != s.charAt(right)) {
+    // return false;
+    // }
+    // left++;
+    // right--;
+    // }
+    // return true;
+    // }
+    // }
+
     static class Solution {
         public boolean isPalindrome(String s) {
-            s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-            int left = 0;
-            int right = s.length() - 1;
+            int left = 0, right = s.length() - 1;
 
             while (left < right) {
-                if (s.charAt(left) != s.charAt(right)) {
-                    return false;
+                char a = s.charAt(left);
+                char b = s.charAt(right);
+
+                // Convert uppercase to lowercase
+                if (a >= 'A' && a <= 'Z')
+                    a = (char) (a + 32);
+                if (b >= 'A' && b <= 'Z')
+                    b = (char) (b + 32);
+
+                // Skip non-alphanumeric
+                if (!((a >= 'a' && a <= 'z') || (a >= '0' && a <= '9'))) {
+                    left++;
+                    continue;
                 }
+                if (!((b >= 'a' && b <= 'z') || (b >= '0' && b <= '9'))) {
+                    right--;
+                    continue;
+                }
+
+                // Compare
+                if (a != b)
+                    return false;
+
                 left++;
                 right--;
             }
+
             return true;
         }
     }
